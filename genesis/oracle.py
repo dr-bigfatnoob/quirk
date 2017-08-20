642fdba60ea167fd40c5f8cec5ef5fbc116c0771
George Mathew <george.meg91@gmail.com>
2017-08-19 22:51:24 -0400

    Generate random positive and negative examples

--- genesis/oracle.py
+++ genesis/oracle.py
@@ -3,10 +3,16 @@ import sys
 import os
 sys.path.append(os.path.abspath("."))
 sys.dont_write_bytecode = True
+import random
 from utils.lib import O
-from genesis.gen_utils import Statement
+from genesis.gen_utils import Statement, Vocabulary, choice
 
 __author__ = "bigfatnoob"
+
+
+def sample(collection, size):
+  size = min(size, len(collection))
+  return random.sample(collection, size)
 
 
 class Oracle(O):
@@ -16,17 +22,46 @@ class Oracle(O):
   def validate(self, query):
     raise NotImplemented("Has to be implemented in sub class.")
 
+  def sample_positive_examples(self, size):
+    raise NotImplemented("Has to be implemented in sub class.")
+
+  def sample_negative_examples(self, size):
+    raise NotImplemented("Has to be implemented in sub class.")
+
 
 class StatementOracle(Oracle):
-
   def __init__(self, statements):
     Oracle.__init__(self)
     self.statements = set()
+    self.vocabulary = None
+    nodes = set()
+    edges = set()
     for statement in statements:
-      if isinstance(statement, str):
-        self.statements.add(Statement.from_string(statement))
-      else:
-        self.statements.add(statement)
+      stmt = statement
+      if isinstance(stmt, str):
+        stmt = Statement.from_string(stmt)
+      nodes.add(stmt.source)
+      edges.add(stmt.relation)
+      nodes.add(stmt.target)
+      self.statements.add(stmt)
+    self.vocabulary = Vocabulary(nodes, edges)
 
   def validate(self, query):
     return query in self.statements
+
+  def sample_positive_examples(self, size):
+    return list(sample(self.statements, size))
+
+  def sample_negative_examples(self, size):
+    samples = set()
+    while len(samples) < size:
+      target = choice(self.vocabulary.nodes)
+      relation = choice(self.vocabulary.edges)
+      source = target
+      while source == target:
+        source = choice(self.vocabulary.nodes)
+      stmt = Statement(source, relation, target)
+      if stmt in self.statements or stmt in samples:
+        continue
+      samples.add(stmt)
+    return list(samples)
--- genesis/quirk_adapter.py
+++ genesis/quirk_adapter.py
@@ -7,12 +7,13 @@ sys.dont_write_bytecode = True
 from language import template as gt
 from genesis import gen_utils
 import networkx as nx
+import pydot as dot
 
 __author__ = "bigfatnoob"
 
 
 def get_node_name(node):
-  return node.name + "_" + node.id
+  return "%s_%d" % (node.name, node.id)
 
 
 def convert(q_model):
@@ -41,9 +42,26 @@ def convert(q_model):
   return points
 
 
+def draw(graph, fig_name):
+  dot_graph = dot.Dot(graph_type='digraph', rankdir="BT")
+  for edge in graph.edges(data=True):
+    style = 'dashed' if edge[2]['relation'] == 'or' else 'solid'
+    dot_graph.add_edge(dot.Edge(edge[0], edge[1], style=style))
+  dot_graph.write(fig_name, format='png')
+
+
 def test_quirk_conversion(model_name):
+  from oracle import StatementOracle
   print("# %s" % model_name)
   from language.parser import Parser
-  from language.mutator import Mutator
   mdl = Parser.from_file("models/quirk/%s.str" % model_name)
   points = convert(mdl)
+  for i, point in enumerate(points):
+    oracle = StatementOracle(point.statements)
+    positive_examples = oracle.sample_positive_examples(2)
+    negative_examples = oracle.sample_negative_examples(2)
+    draw(point.decisions, "genesis/adapter_%d.png" % i)
+
+
+if __name__ == "__main__":
+  test_quirk_conversion("AOWS")
