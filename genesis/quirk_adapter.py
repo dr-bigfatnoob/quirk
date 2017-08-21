f5025d7139adaf06380c429b436ccbf1e7611a16
George Mathew <george.meg91@gmail.com>
2017-08-21 16:47:50 -0400

    Oracles

--- genesis/quirk_adapter.py
+++ genesis/quirk_adapter.py
@@ -5,6 +5,7 @@ import os
 sys.path.append(os.path.abspath("."))
 sys.dont_write_bytecode = True
 from language import template as gt
+from utils import cache
 from genesis import gen_utils
 import networkx as nx
 import pydot as dot
@@ -13,7 +14,8 @@ __author__ = "bigfatnoob"
 
 
 def get_node_name(node):
-  return "%s_%d" % (node.name, node.id)
+  # return "%s_%d" % (node.name, node.id)
+  return node.name
 
 
 def convert(q_model):
@@ -43,25 +45,32 @@ def convert(q_model):
 
 
 def draw(graph, fig_name):
+  directory = fig_name.rsplit("/", 1)[0]
+  cache.mkdir(directory)
   dot_graph = dot.Dot(graph_type='digraph', rankdir="BT")
+  dot_graph.set_node_defaults(fontsize=6, margin=0, fixedsize='false', shape='box')
   for edge in graph.edges(data=True):
     style = 'dashed' if edge[2]['relation'] == 'or' else 'solid'
     dot_graph.add_edge(dot.Edge(edge[0], edge[1], style=style))
   dot_graph.write(fig_name, format='png')
 
 
-def test_quirk_conversion(model_name):
-  from oracle import StatementOracle
-  print("# %s" % model_name)
+def dump_model(name):
+  print("# %s" % name)
   from language.parser import Parser
-  mdl = Parser.from_file("models/quirk/%s.str" % model_name)
+  mdl = Parser.from_file("models/quirk/%s.str" % name)
   points = convert(mdl)
+  file_name = "genesis/oracles/%s/objectives.pkl" % name
+  cache.save_file(file_name, points)
   for i, point in enumerate(points):
-    oracle = StatementOracle(point.statements)
-    positive_examples = oracle.sample_positive_examples(2)
-    negative_examples = oracle.sample_negative_examples(2)
-    draw(point.decisions, "genesis/adapter_%d.png" % i)
+    fig_name = "genesis/oracles/%s/obj_%d.png" % (name, i)
+    draw(point.decisions, fig_name)
 
 
 if __name__ == "__main__":
-  test_quirk_conversion("AOWS")
+  dump_model("AOWS")
+  dump_model("BSPDM")
+  dump_model("CBA")
+  dump_model("ECS")
+  dump_model("FDM")
+  dump_model("SAS")
--- utils/cache.py
+++ utils/cache.py
@@ -3,6 +3,7 @@ import sys
 import os
 sys.path.append(os.path.abspath("."))
 sys.dont_write_bytecode = True
+from utils.lib import mkdir
 import cPickle as cPkl
 
 
@@ -12,5 +13,7 @@ def load_file(file_name):
 
 
 def save_file(file_name, obj):
+  directory = file_name.rsplit("/", 1)[0]
+  mkdir(directory)
   with open(file_name, "wb") as f:
     cPkl.dump(obj, f, cPkl.HIGHEST_PROTOCOL)
